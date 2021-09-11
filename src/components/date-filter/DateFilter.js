c461797c391875cdd02a28c71dc5d5e370d002a1
TrueRacoon <georgebersenev@gmail.com>
2021-09-12 01:14:27 +0700

    Add departure expand button listener.

--- src/components/date-filter/DateFilter.js
+++ src/components/date-filter/DateFilter.js
@@ -7,6 +7,9 @@ class DateFilter {
     this.arrivalExpandButton = this.dateFilterDom.querySelector(
       '.js_date-filter__arrival .js_input__button_type_dropdown',
     );
+    this.departureExpandButton = this.dateFilterDom.querySelector(
+      '.js_date-filter__departure .js_input__button_type_dropdown',
+    );
     this.calendarContainer = this.dateFilterDom.querySelector(
       '.js-date-filter__calendar-container',
     );
@@ -15,10 +18,19 @@ class DateFilter {
 
   _bindEventListeners() {
     this.arrivalExpandButton.addEventListener('click', this._handleArrivalExpandButtonClick);
+    this.departureExpandButton.addEventListener('click', this._handleDepartureExpandButtonClick);
   }
 
   _handleArrivalExpandButtonClick = () => {
     this.calendarContainer.classList.toggle('date-filter__calendar-container_opened');
+    this.arrivalExpandButton.classList.toggle('input__button_type_dropdown_turned');
+    this.departureExpandButton.classList.remove('input__button_type_dropdown_turned');
+  }
+
+  _handleDepartureExpandButtonClick = () => {
+    this.calendarContainer.classList.toggle('date-filter__calendar-container_opened');
+    this.departureExpandButton.classList.toggle('input__button_type_dropdown_turned');
+    this.arrivalExpandButton.classList.remove('input__button_type_dropdown_turned');
   }
 }
 
