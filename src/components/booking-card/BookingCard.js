02430c5990e4b8b1073916de61d3f1289680612e
TrueRacoon <georgebersenev@gmail.com>
2021-09-27 11:22:09 +0700

    Work on booking card component

--- src/components/booking-card/BookingCard.js
+++ src/components/booking-card/BookingCard.js
@@ -1,11 +1,55 @@
 class BookingCard {
-  constructor(bookingCardDom) {
+  constructor(bookingCardDom, dateFilter, guestPicker) {
     this.bookingCardDom = bookingCardDom;
+    this.dateFilter = dateFilter;
+    this.guestPicker = guestPicker;
   }
 
   init() {
-    
+    this.priceValue = this.bookingCardDom.querySelector('.js-booking-card__price-value');
+    this.costDetailsList = this.bookingCardDom.querySelector('.js-booking-card__cost-details-list');
+    this.baseCostDetails = this.bookingCardDom.querySelector('.js-booking-card__base-cost-details');
+    this.submitButton = this.bookingCardDom.querySelector('.js-booking-card__submit-button');
+    this.totalBaseCost = this.bookingCardDom.querySelector('.js-booking-card__total-base-cost');
+    this.additionalServicesFees = this.bookingCardDom.querySelector('.js-booking-card__additional-services-fees');
+    this.discount = this.bookingCardDom.querySelector('.js-booking-card__discount');
+    this.totalCostBlock = this.bookingCardDom.querySelector('.js-booking-card__total-cost-block');
+    this.totalCost = this.bookingCardDom.querySelector('.js-booking-card__total-cost');
+    this._handleState();
   }
+
+  _handleState = () => {
+    const guestCounter = this.guestPicker.getCountersSum();
+    const needHideCostAndSubmitButton = (
+      guestCounter === 0
+      || !this.dateFilter.calendar.areBothDatesSelected()
+    );
+    if (needHideCostAndSubmitButton) {
+      this.costDetailsList.classList.add('booking-card__cost-details-list_hidden');
+      this.totalCostBlock.classList.add('booking-card__total-cost-block_hidden');
+      this.submitButton.classList.add('booking-card__submit-button_hidden');
+      return;
+    }
+    const priceValue = parseInt(this.priceValue.innerText.split(' ').join(''), 10);
+    const discount = parseInt(this.discount.innerText.slice(22).split(' ').join(''), 10);
+    const hostelStay = this._getHotelStay();
+    const baseCost = priceValue * hostelStay;
+    const additionalServicesFees = guestCounter * 100;
+    const totalCost = baseCost - discount + additionalServicesFees;
+    this.baseCostDetails.innerText = `${this.priceValue.innerText} x ${hostelStay}`;
+    this.totalBaseCost.innerText = this._formatNumber(baseCost);
+    this.additionalServicesFees.innerText = this._formatNumber(additionalServicesFees);
+    this.totalCost.innerText = this._formatNumber(totalCost);
+  }
+
+  _formatNumber = (sum) => sum.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1\xa0').concat('₽');
+
+  _getHotelStay = () => (
+    (
+      this.dateFilter.calendar.getSecondSelectedDate().getTime()
+      - this.dateFilter.calendar.getFirstSelectedDate().getTime()
+    ) / 86400000
+  );
 }
 
 export default BookingCard;
--- src/components/booking-card/index.js
+++ src/components/booking-card/index.js
@@ -1,8 +1,15 @@
 import BookingCard from './BookingCard';
+import createAndInitDateFilter from '../date-filter';
+import CountPicker from '../count-picker/CountPicker';
 
 const bookingCards = document.querySelectorAll('.js-booking-card');
 
 bookingCards.forEach((bookingCardDom) => {
-  const bookingCard = new BookingCard(bookingCardDom);
+  const dateFilterDom = bookingCardDom.querySelector('.js-date-filter');
+  const dateFilter = createAndInitDateFilter(dateFilterDom);
+  const guestPickerDom = bookingCardDom.querySelector('.js-count-picker');
+  const guestPicker = new CountPicker(guestPickerDom);
+  guestPicker.init();
+  const bookingCard = new BookingCard(bookingCardDom, dateFilter, guestPicker);
   bookingCard.init();
 });
