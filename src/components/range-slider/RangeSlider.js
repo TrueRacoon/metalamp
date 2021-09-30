39d4dc704a9e4ecc38851b5f5e952e5f967ed848
TrueRacoon <georgebersenev@gmail.com>
2021-10-01 04:09:46 +0700

    Add filler for selected range. Fixes for FF and Chrome compatibility.

--- src/components/range-slider/RangeSlider.js
+++ src/components/range-slider/RangeSlider.js
@@ -7,26 +7,31 @@ class RangeSlider {
   }
 
   _init() {
+    this.width = this.rangeSliderDom.getBoundingClientRect().width;
     this.lowerInput = this.rangeSliderDom.querySelector('.js-range-slider__input_lower');
     this.upperInput = this.rangeSliderDom.querySelector('.js-range-slider__input_upper');
-    this.sliderRange = this.rangeSliderDom.querySelector('.js-range-slider__range-label');
+    this.sliderRangeLabel = this.rangeSliderDom.querySelector('.js-range-slider__range-label');
     this.step = parseInt(this.lowerInput.step, 10);
-    this.stepMultiplierForNoCrossing = Math.ceil(this.rangeSliderDom.getBoundingClientRect().width / (thumbWidth * 2));
-    this.offset = this.step * (this.stepMultiplierForNoCrossing / 2);
+    this.stepMultiplierForNoCrossing = Math.ceil(this.width / (thumbWidth * 2));
+    this.offset = this.step * this.stepMultiplierForNoCrossing;
+    this.halfOffset = this.offset / 2;
     this._setInputsInitialValues();
+    this.inputMin = parseInt(this.lowerInput.min, 10);
+    this.inputMax = parseInt(this.upperInput.max, 10);
+    this._updateSliderFill();
     this._bindEventListeners();
   }
 
   _setInputsInitialValues = () => {
-    this.lowerInput.min = parseInt(this.sliderRange.dataset.minValue, 10) - this.offset;
-    this.upperInput.min = parseInt(this.sliderRange.dataset.minValue, 10) - this.offset;
-    this.lowerInput.max = parseInt(this.sliderRange.dataset.maxValue, 10) + this.offset;
-    this.upperInput.max = parseInt(this.sliderRange.dataset.maxValue, 10) + this.offset;
-    this.lowerInput.value = parseInt(this.sliderRange.dataset.initialLowerValue, 10) - this.offset;
-    this.upperInput.value = parseInt(this.sliderRange.dataset.initialUpperValue, 10) + this.offset;
-    this.sliderRange.innerText = (
-      `${this._formatSum(parseInt(this.sliderRange.dataset.initialLowerValue, 10))}`
-      + ` - ${this._formatSum(parseInt(this.sliderRange.dataset.initialUpperValue, 10))}`
+    this.lowerInput.min = parseInt(this.sliderRangeLabel.dataset.minValue, 10) - this.halfOffset;
+    this.upperInput.min = parseInt(this.sliderRangeLabel.dataset.minValue, 10) - this.halfOffset;
+    this.lowerInput.max = parseInt(this.sliderRangeLabel.dataset.maxValue, 10) + this.halfOffset;
+    this.upperInput.max = parseInt(this.sliderRangeLabel.dataset.maxValue, 10) + this.halfOffset;
+    this.lowerInput.value = parseInt(this.sliderRangeLabel.dataset.initialLowerValue, 10) - this.halfOffset;
+    this.upperInput.value = parseInt(this.sliderRangeLabel.dataset.initialUpperValue, 10) + this.halfOffset;
+    this.sliderRangeLabel.innerText = (
+      `${this._formatSum(parseInt(this.sliderRangeLabel.dataset.initialLowerValue, 10))}`
+      + ` - ${this._formatSum(parseInt(this.sliderRangeLabel.dataset.initialUpperValue, 10))}`
     );
   }
 
@@ -38,38 +43,54 @@ class RangeSlider {
   _handleLowerInputInput = () => {
     this.lowerValue = parseInt(this.lowerInput.value, 10);
     this.upperValue = parseInt(this.upperInput.value, 10);
-    this.upperMax = parseInt(this.upperInput.max, 10);
-    const needOffsetUpperValue = this.lowerValue >= this.upperValue - this.step * this.stepMultiplierForNoCrossing;
-    const needOffsetLowerValue = this.upperValue >= this.upperMax - this.step * this.stepMultiplierForNoCrossing;
+    const needOffsetUpperValue = this.lowerValue >= this.upperValue - this.offset;
+    const needOffsetLowerValue = (
+      needOffsetUpperValue
+      && this.upperValue >= this.inputMax - this.offset
+    );
     if (needOffsetUpperValue) {
-      this.upperInput.value = this.lowerValue + this.step * this.stepMultiplierForNoCrossing;
+      this.upperInput.value = this.lowerValue + this.offset;
     }
     if (needOffsetLowerValue) {
-      this.lowerInput.value = this.upperValue - this.step * this.stepMultiplierForNoCrossing;
+      this.lowerInput.value = this.upperValue - this.offset;
     }
     this._updateRangeLabel();
+    this._updateSliderFill();
   }
 
   _handleUpperInputInput = () => {
     this.lowerValue = parseInt(this.lowerInput.value, 10);
     this.upperValue = parseInt(this.upperInput.value, 10);
-    this.lowerMin = parseInt(this.lowerInput.min, 10);
-    const needOffsetLowerValue = this.upperValue <= this.lowerValue + this.step * this.stepMultiplierForNoCrossing;
-    const needOffsetUpperValue = this.lowerValue <= this.lowerMin + this.step * this.stepMultiplierForNoCrossing;
+    const needOffsetLowerValue = this.upperValue <= this.lowerValue + this.offset;
+    const needOffsetUpperValue = (
+      needOffsetLowerValue
+      && this.lowerValue <= this.inputMin + this.offset
+    );
     if (needOffsetLowerValue) {
-      this.lowerInput.value = this.upperValue - this.step * this.stepMultiplierForNoCrossing;
+      this.lowerInput.value = this.upperValue - this.offset;
     }
     if (needOffsetUpperValue) {
-      this.upperInput.value = this.lowerValue + this.step * this.stepMultiplierForNoCrossing;
+      this.upperInput.value = this.lowerValue + this.offset;
     }
     this._updateRangeLabel();
+    this._updateSliderFill();
   }
 
   _updateRangeLabel = () => {
-    this.sliderRange.innerText = (
-      `${this._formatSum(parseInt(this.lowerInput.value, 10) + this.offset)}`
-      + ` - ${this._formatSum(parseInt(this.upperInput.value, 10) - this.offset)}`
+    this.sliderRangeLabel.innerText = (
+      `${this._formatSum(parseInt(this.lowerInput.value, 10) + this.halfOffset)}`
+      + ` - ${this._formatSum(parseInt(this.upperInput.value, 10) - this.halfOffset)}`
     );
+  }
+
+  _updateSliderFill = () => {
+    this.lowerValue = parseInt(this.lowerInput.value, 10);
+    this.upperValue = parseInt(this.upperInput.value, 10);
+    this.leftPoint = ((this.lowerValue - this.inputMin) / (this.inputMax - this.inputMin)) * this.width;
+    this.rightPoint = ((this.inputMax - this.upperValue) / (this.inputMax - this.inputMin)) * this.width;
+    this.sliderFill = this.rangeSliderDom.querySelector('.js-range-slider__fill');
+    this.sliderFill.style.left = `${this.leftPoint + (thumbWidth / 2) * (this.leftPoint > this.width / 2 ? -1 : 1)}px`;
+    this.sliderFill.style.right = `${this.rightPoint + (thumbWidth / 2) * (this.rightPoint > this.width / 2 ? -1 : 1)}px`;
   }
 
   _formatSum = (sum) => (
