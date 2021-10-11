88d575e359e255e586bc49d0c6bf451e4b711815
TrueRacoon <georgebersenev@gmail.com>
2021-10-11 18:33:34 +0700

    Work on form elements page

--- src/pages/form-elements/form-elements.js
+++ src/pages/form-elements/form-elements.js
@@ -3,3 +3,5 @@ import './form-elements.scss';
 import '../ui-kit/ui-kit.scss';
 import '../../components/date-filter';
 import '../../components/count-picker';
+import '../../components/expandable-checkbox-list';
+import '../../components/range-slider';
