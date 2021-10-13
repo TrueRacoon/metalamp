0145d8eec5fcce5ecd45b00369e2ea59dc107a3e
TrueRacoon <georgebersenev@gmail.com>
2021-10-14 03:14:47 +0700

    Add resolve-url-loader. Replace all paths to image files in css with relative ones. Use background shorthand property instead of background style properties.

--- webpack.common.js
+++ webpack.common.js
@@ -70,6 +70,10 @@ module.exports = {
             options: { sourceMap: true },
           },
           {
+            loader: 'resolve-url-loader',
+            options: { sourceMap: true },
+          },
+          {
             loader: 'sass-loader',
             options: { sourceMap: true },
           },
